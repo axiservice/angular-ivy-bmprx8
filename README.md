ec0fb9bdeabd2f194b9ab10282cdca2952b3962a
axiservice <axi.tms@gmail.com>
2022-08-06 21:49:56 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-bmprx8
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-bmprx8)
---
+++ src/app/menu/menu.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.css'],
+})
+export class MenuComponent implements OnInit {
+  link_menu_1: string;
+  link_menu_2: string;
+  link_menu_3: string;
+
+  constructor() {
+    this.link_menu_1 = 'Treni';
+    this.link_menu_2 = 'Preferiti';
+    this.link_menu_3 = 'Login';
+  }
+
+  ngOnInit() {}
+}
